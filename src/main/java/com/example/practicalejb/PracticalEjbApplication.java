0f21ef5f9c72084aa41ba16cadda739351f9ebe6
hoangsonhv <hvsson102@gmail.com>
2022-05-27 09:14:59 +0700

    Practical EJB

---
+++ src/main/java/com/example/practicalejb/PracticalEjbApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicalejb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalEjbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticalEjbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicalejb/PracticalEjbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicalejb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalEjbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
